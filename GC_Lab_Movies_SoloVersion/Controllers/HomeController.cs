78c99a0db7019f12b715dfad88835a11c84c29a7
antonio <antoniomanzari@gmail.com>
2021-03-04 08:59:13 -0500

    made movie grid responsive

--- GC_Lab_Movies_SoloVersion/Controllers/HomeController.cs
+++ GC_Lab_Movies_SoloVersion/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace GC_Lab_Movies_SoloVersion.Controllers
                 {
                     MovieModel = movieDAL.GetMovieById(i),
                     BaseURL = movieDAL.GetBaseURL(),
-                    ImageSizeParam = movieDAL.GetImageSizeParamXSmall() // not sure where the image size decision should be made.
+                    ImageSizeParam = movieDAL.GetImageSizeParamMedium() // This is where the image size decision is made. Maybe this should be done elsewhere?
                 };
 
                 movieListTest.Add(singleMovie);
